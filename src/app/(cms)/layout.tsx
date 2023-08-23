9ccb7ba1e28accac47c7a80a6e2c48e1ba8aadd1
igorsilvaj <igorjsilvabiz@gmail.com>
2023-08-23 19:35:41 -0300

    :fire:feat: remove header from layout

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <h2>CMS Layout</h2>
       {children}
     </div>
   );
